50f84bfc78eb8bba6f471488ac1c107854e443d2
Mathananthi <madhansanjay2012@gmail.com>
2023-10-12 14:15:07 +0530

    Success

--- sws/sws/asgi.py
+++ sws/sws/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for snmp project.
+ASGI config for sws project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snmp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sws.settings')
 
 application = get_asgi_application()
--- sws/sws/wsgi.py
+++ sws/sws/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for snmp project.
+WSGI config for sws project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snmp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sws.settings')
 
 application = get_wsgi_application()
